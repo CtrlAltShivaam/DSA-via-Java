4f0d284b9bc16baef45fb006f1235085c5cc8917
Shivam Sharma <bwubts20122@brainwareumiversity.ac.in>
2022-07-24 21:46:46 +0530

    Binary Tree

---
+++ Binary Tree/BinaryTree_Count_of_nodes.java
@@ -0,0 +1,46 @@
+public class BinaryTree_Count_of_nodes {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int idx=-1;
+
+        public Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1)
+            return null;
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+
+            return newNode;
+
+        }
+
+    }
+    public static int countofnodes(Node root){
+        if(root==null)
+        return 0;
+
+        int leftNodes = countofnodes(root.left);
+        int rightNodes = countofnodes(root.right);
+        return leftNodes + rightNodes + 1;
+        
+    }
+    public static void main(String[] args) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root= tree.buildTree(nodes);
+        System.out.println(countofnodes(root));
+        
+    }
+}
